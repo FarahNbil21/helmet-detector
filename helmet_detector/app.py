d002543bdbe14eb37420fe53b12cdc8e7d7a51a7
Farah Elprince <141284079+FarahNbil21@users.noreply.github.com>
2026-04-23 19:18:31 +0200

    Add files via upload

---
+++ helmet_detector/app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from ultralytics import YOLO
+from PIL import Image
+import numpy as np
+
+st.set_page_config(page_title="Helmet Detector", page_icon="⛑️")
+st.title("Helmet & Safety Vest Detector")
+st.write("Upload a photo or use the camera to verify your safety equipment")
+model = YOLO('best.pt')
+
+option = st.radio("Choose input method:", ["📸 Upload Image", "📷 Camera"])
+
+if option == "📸 Upload Image":
+    uploaded = st.file_uploader("Upload Image", type=['jpg','png','jpeg'])
+    if uploaded:
+        image = Image.open(uploaded)
+        st.image(image, caption="Original Image")
+        with st.spinner("Analyzing..."):
+            results = model.predict(np.array(image), conf=0.5)
+            st.image(results[0].plot(), caption="Result ")
+
+elif option == "Camera":
+    img = st.camera_input("Capture Image")
+    if img:
+        image = Image.open(img)
+        with st.spinner("Analyzing..."):
+            results = model.predict(np.array(image), conf=0.5)
+            st.image(results[0].plot(), caption="Result")
